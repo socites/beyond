79dc70c9b2067d092da870050022f7c35aa7003b
Enrique Box <ebox@socites.com>
2016-04-04 17:53:21 -0300

    minor bug fixes

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -1,7 +1,14 @@
 module.exports = function () {
     "use strict";
 
+    let childProcess = require('child_process');
+    let cwd = require('path').join(process.cwd(), 'lib/modules/applications/client/vendor/0.0.1');
+
     // cd lib/modules/applications/client/vendor/0.0.1 && bower cache clean && bower install
-    console.log('Please make a manual bower install inside the vendor libraries before using beyond');
+    console.log('cd ' + cwd);
+    childProcess.execSync('bower install', {
+        'cwd': cwd,
+        'stdio': [0, 1, 2]
+    });
 
 };
