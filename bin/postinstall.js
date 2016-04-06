888d1dabdde1d9e226531a15a1a9722adf224f71
jmarquez <flexbed@gmail.com>
2016-04-06 15:12:45 -0300

    Postinstall detect root

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -5,7 +5,7 @@ module.exports = function () {
     let cwd = require('path').join(process.cwd(), 'lib/modules/applications/client/vendor/0.0.1');
 
     // cd lib/modules/applications/client/vendor/0.0.1 && bower cache clean && bower install
-    console.log('cd ' + cwd);
+    //console.log('cd ' + cwd);
     //childProcess.execSync('bower install', {
     childProcess.execSync('if [[ "$(id -u)" != "0" ]]; then sudo bower install --allow-root; else bower install --allow-root; fi', {
         'cwd': cwd,
