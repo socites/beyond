0dbc848f7eb139ba75afa4082ca4fd0f2fb35805
Enrique Box <ebox@socites.com>
2016-05-17 16:27:07 -0300

    minor bug fixes

--- bin/postinstall.js
+++ bin/postinstall.js
@@ -1,33 +1,27 @@
 module.exports = function () {
     "use strict";
 
-    /*
-     let childProcess = require('child_process');
-     let cwd = require('path').join(process.cwd(), 'lib/modules/applications/client/vendor/0.0.1');
-     */
+    let childProcess = require('child_process');
+    let cwd = require('path').join(process.cwd(), 'lib/modules/applications/client/vendor/0.0.1');
 
     // cd lib/modules/applications/client/vendor/0.0.1 && bower cache clean && bower install
-    /*
-     console.log('cd ' + cwd);
+    console.log('cd ' + cwd);
 
-     let cmd = 'if [[ "$(id -u)" != "0" ]]; ' +
-     'then sudo chown -R $USER ~/.config/configstore && ' +
-     'bower install; else bower install --allow-root; fi';
-     */
+    let cmd = 'if [[ "$(id -u)" != "0" ]]; ' +
+        'then sudo chown -R $USER ~/.config/configstore && ' +
+        'bower install; else bower install --allow-root; fi';
 
-    /* Try to find out OS*/
-    /*
-     let isWin = /^win/.test(process.platform);
+    /* Try to find out OS */
+    let isWin = /^win/.test(process.platform);
 
-     if (isWin) {
-     cmd = 'bower install --allow-root';
-     }
+    if (isWin) {
+        cmd = 'bower install --allow-root';
+    }
 
-     childProcess.execSync(
-     cmd, {
-     'cwd': cwd,
-     'stdio': [0, 1, 2]
-     });
-     */
+    childProcess.execSync(
+        cmd, {
+            'cwd': cwd,
+            'stdio': [0, 1, 2]
+        });
 
 };
