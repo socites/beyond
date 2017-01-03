3cdf405998f0d525b20b58278eef5c0a11bab23b
Enrique Box <ebox@socites.com>
2017-01-03 15:02:30 -0300

    Pulldown module added to ui library

--- app/dynamic/js/page.js
+++ app/dynamic/js/page.js
@@ -7,6 +7,8 @@ function Page($container, parameter, dependencies) {
         $container.html(module.render('index', module.texts));
 
         var control = $container.find('beyond-example-control').get(0);
+        control.parentScroller = null;
+
         console.log(control);
         window.asd = control;
 
