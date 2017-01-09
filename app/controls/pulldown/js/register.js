2a9fedfb4fd5c21b0619c64dcaa79ec227296f38
Enrique Box <ebox@socites.com>
2017-01-09 17:18:44 -0300

    Support custom properties on Control and PullDown controls

--- app/controls/pulldown/js/register.js
+++ app/controls/pulldown/js/register.js
@@ -2,14 +2,36 @@
     "use strict";
 
     var dependencies = module.dependencies;
+    var sna;
+
+    function ExampleBehavior() {
+
+        var control;
+
+        this.created = function () {
+            control = this;
+        };
+
+        this.setCoolAttribute = function (value) {
+            console.log('cool attribute is set', value);
+        };
+
+        this.properties = {
+            'coolAttribute': {
+                'type': String,
+                'observer': 'setCoolAttribute'
+            }
+        };
+
+    }
 
     beyond.ui.PullDown('beyond-pulldown-control', dependencies, function () {
 
-        var sna = new SNA();
+        sna = new SNA();
 
         var List = react.list;
         return module.React.createElement(List, {'sna': sna});
 
-    });
+    }, [new ExampleBehavior()]);
 
 })();
