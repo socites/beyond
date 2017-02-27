9d38cbffc498be5c47a3e17424cf4cee8c85e5df
Enrique Box <ebox@socites.com>
2017-02-27 19:51:39 -0300

    Screens

--- applications/build/build.js
+++ applications/build/build.js
@@ -1,7 +1,7 @@
-(function (Beyond) {
+(function (beyond) {
     "use strict";
 
-    let config, beyond;
+    let config;
 
     config = {
         'paths': {
@@ -10,24 +10,40 @@
         },
         'libraries': {
             'library1': './library1/library.json'
+        },
+        'applications': {
+            'application1': './application1/application.json'
         }
     };
 
-    beyond = new Beyond(config, {'environment': 'development'});
-    beyond.build({
-        'libraries': {
-            'library1': {
-                'mode': 'beyond',
-                'version': 'v1',
-                'npm': {
-                    'version': '0.1',
-                    'publish': true
-                },
-                'path': './',
-                'modules': {
-                    'client': true,
-                    'server': true
+    let builder = new beyond.Builder(config, {'environment': 'development'});
+
+    function buildLibraries() {
+
+        builder.build({
+            'libraries': {
+                'library1': {
+                    'mode': 'beyond',
+                    'version': 'v1',
+                    'npm': {
+                        'version': '0.1',
+                        'publish': true
+                    },
+                    'path': './',
+                    'modules': {
+                        'client': true,
+                        'server': true
+                    }
                 }
+            }
+        });
+
+    }
+
+    builder.build({
+        'applications': {
+            'application1': {
+                'mode': 'phonegap'
             }
         }
     });
