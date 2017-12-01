6816c242c8091ab3622e900a5b5973290610e75b
Enrique Box <ebox@socites.com>
2017-12-01 11:43:58 -0300

    Improving Types

--- types/index.js
+++ types/index.js
@@ -4,16 +4,31 @@
 module.exports = function () {
     "use strict";
 
-    let registered = new Map();
-    Object.defineProperty(this, 'registered', {
+    let types = new Map();
+    Object.defineProperty(this, 'types', {
         'get': function () {
-            return registered;
+            return types;
         }
     });
 
-    registered.set('code', require('./registered/code'));
-    registered.set('page', require('./registered/page'));
-    registered.set('control', require('./registered/control'));
-    registered.set('icons', require('./registered/icons'));
+    let processors = new Map();
+    Object.defineProperty(this, 'processors', {
+        'get': function () {
+            return processors;
+        }
+    });
+
+    types.set('code', require('./registered/code'));
+    types.set('page', require('./registered/page'));
+    types.set('control', require('./registered/control'));
+    types.set('icons', require('./registered/icons'));
+
+    processors.set('css', require('./processors/css.js'));
+    processors.set('html', require('./processors/html.js'));
+    processors.set('js', require('./processors/js.js'));
+    processors.set('jsx', require('./processors/jsx.js'));
+    processors.set('less', require('./processors/less.js'));
+    processors.set('mustache', require('./processors/mustache.js'));
+    processors.set('txt', require('./processors/txt.js'));
 
 };
