f765b06375059fd36f2bc3ce5b941d5cc051ff99
Enrique Box <ebox@socites.com>
2017-12-07 18:15:01 -0300

    First version of Progressing Model

--- application/model/client/libraries/item.js
+++ application/model/client/libraries/item.js
@@ -1,4 +1,14 @@
-function Library() {
+function Library(id) {
     "use strict";
+
+    var item = new module.model.Item(this, id);
+
+    var properties = item.properties;
+    properties.define(['name', 'dirname']);
+
+    this.update = function (data) {
+        properties.name = data.name;
+        properties.dirname = data.dirname;
+    };
 
 }
