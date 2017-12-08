12b806767f795cbcb9d108e163dbb5e3475c8de5
Enrique Box <ebox@socites.com>
2017-12-08 20:17:35 -0300

    Compiling applications & libraries from beyond application

--- application/model/server/libraries/compile.js
+++ application/model/server/libraries/compile.js
@@ -1,6 +1,30 @@
 module.exports = require('async')(function *(resolve, reject, params, context) {
     "use strict";
 
+    let builder = require('path').join(require('main.lib'), 'builder/');
+    builder = require(builder);
+
+    let specs = {
+        'libraries': {
+            'beyond': {
+                'client': false,
+                'server': true,
+                'mode': 'beyond',
+                'npm': {
+                    'version': '0.1'
+                },
+                'path': './',
+                'modules': {
+                    'client': true,
+                    'server': true
+                }
+            }
+        }
+    };
+
+    let runtime = 'development';
+
+    yield builder.build('./build', context.modules, specs, runtime);
     resolve();
 
 });
