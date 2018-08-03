cc0d55a0a09d49edf8fba40ab9d0e24ce8161ee7
enriquebox <ebox@socites.com>
2018-08-03 10:02:43 -0300

    Refactoring the module object

--- types/types/js/index.js
+++ types/types/js/index.js
@@ -1,54 +1,19 @@
 /**
- * Compiles typescript
+ * Compiles js
  */
 module.exports = function (module, config, error) {
     'use strict';
 
     let async = require('async');
 
-    Object.defineProperty(this, 'multilanguage', {
-        'get': () => false
-    });
+    Object.defineProperty(this, 'multilanguage', {'get': () => false});
+    Object.defineProperty(this, 'extname', {'get': () => '.js'});
 
-    Object.defineProperty(this, 'extname', {
-        'get': () => '.js'
-    });
-
+    // Add script inside its own function
     let scope = function (code, standalone) {
-
-        // add an extra tab in all lines
-        code = code.replace(/\n/g, '\n    ');
-        code = '    ' + code + '\n';
-
-        // add script inside its own function
-        let output = '';
-
-        if (standalone) {
-            output += '(function () {\n\n';
-        }
-        else {
-            output += '(function (params) {\n\n';
-            output += '    var done = params[1];\n';
-            output += '    var module = params[0];\n\n';
-
-            if (config.dependencies) {
-                output += '    var dependencies = module.dependencies;\n';
-                output += '    module.dependencies.set(' + JSON.stringify(config.dependencies) + ');\n\n';
-            }
-        }
-
-        output += code;
-
-        if (standalone) {
-            output += '})();';
-        }
-        else {
-            output += '    done(\'' + module.ID + '\', \'code\');\n\n';
-            output += '})(beyond.modules.get(\'' + module.ID + '\'));';
-        }
-
-        return output;
-
+        code = code.replace(/\n/g, '\n    '); // Add an extra tab in all lines
+        code = `    ${code}\n`;
+        return `(function () {\n\n${code}})();`;
     };
 
     this.process = async(function* (resolve, reject, language) {
@@ -70,12 +35,9 @@ module.exports = function (module, config, error) {
     });
 
     this.setBuildConfig = async(function* (resolve, reject, json) {
-
         json.id = config.id;
         json.dependencies = config.dependencies;
-
         resolve();
-
     });
 
 };
