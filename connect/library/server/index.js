6b5db4a9d559ca2abdadfce37605f4991c224599
enriquebox <ebox@socites.com>
2018-09-18 17:11:51 -0300

    Refactoring connect

--- connect/library/server/index.js
+++ connect/library/server/index.js
@@ -1,9 +1,7 @@
-let async = require('async');
-
 module.exports = function () {
 
-    this.hello = async(function* (resolve, reject, params, context) {
-        resolve('hello world');
-    });
+    this.hello = async function (params, context) {
+        return 'hello world';
+    };
 
 };
