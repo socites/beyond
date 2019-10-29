fb94de20c6fa759bb450cd08bd2d7c116b6476f6
henrybox <ebox@socites.com>
2019-10-29 19:23:36 -0300

    improving connection errors

--- connect/library/page.js
+++ connect/library/page.js
@@ -1,20 +1,11 @@
 function Page() {
 
-    this.show = function () {
+    this.show = async function () {
 
-        let action = new module.Action('/hello');
+        const action = new module.Action('/hello');
 
-        action.onResponse = function (response) {
-            console.log('on response', response);
-        };
-
-        action.onError = function (error) {
-            console.error('on error', error);
-        };
-
-        action.execute()
-            .then(response => console.log('promise response', response))
-            .catch(error => console.error('promise error', error));
+        const response = await action.execute();
+        console.log(response);
 
     };
 
