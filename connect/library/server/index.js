b373579678bbf098463c7bca8f3a06fd25e5852e
henrybox <ebox@socites.com>
2019-10-31 16:54:59 -0300

    Improving toast message

--- connect/library/server/index.js
+++ connect/library/server/index.js
@@ -1,5 +1,5 @@
 module.exports = function () {
 
-    this.hello = () => new Promise(resolve => setTimeout(resolve('hello world'), 10000));
+    this.hello = () => new Promise(resolve => setTimeout(() => resolve('hello world'), 10000));
 
 };
